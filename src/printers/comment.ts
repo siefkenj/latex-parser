614153d8a5a05a70352bb386ebf3a2f4d068de66
Jason Siefken <siefkenj@gmail.com>
2021-06-07 16:16:31 -0400

    Fixed comments at start of env

--- src/printers/comment.ts
+++ src/printers/comment.ts
@@ -2,26 +2,16 @@ import type { Doc } from "prettier";
 import * as Ast from "../libs/ast-types";
 import * as PrettierTypes from "./prettier-types";
 import {
-    getNodeInfo,
-    softline,
-    fill,
     concat,
-    hardline,
-    line,
-    ESCAPE,
 } from "./common";
 import { printRaw } from "../parsers/parser";
 
 export function printComment(
     path: PrettierTypes.FastPath,
-    print: PrettierTypes.RecursivePrintFunc,
-    options: any
+    _print: PrettierTypes.RecursivePrintFunc,
+    _options: any
 ): Doc {
     const node = path.getNode() as Ast.Comment;
-    const { renderInfo, previousNode, nextNode, referenceMap } = getNodeInfo(
-        node,
-        options
-    );
 
     // If a comment is on the same line as other content and it has leading whitespace,
     // add a single whitespace token.
@@ -30,7 +20,7 @@ export function printComment(
         leadingWhitespace = " ";
     }
 
-    const content: PrettierTypes.Doc[] = [
+    const content: Doc[] = [
         leadingWhitespace,
         "%" + printRaw(node.content),
     ];
